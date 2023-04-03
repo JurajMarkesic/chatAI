6bf3790ea4bfd825db96be556f6a85217f03ea75
JurajMarkesic <jurajmarkesic@googlemail.com>
2023-04-03 19:56:16 +0200

    console.log chat output, added env var to example.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,59 +1,53 @@
-import { fail, redirect, type ActionFailure } from '@sveltejs/kit';
+import { fail, type ActionFailure } from '@sveltejs/kit';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import { AuthApiError } from '@supabase/supabase-js';
+import { SECRET_OPENAI_KEY } from '$env/static/private';
 import type { Actions } from './$types';
 import { Configuration, OpenAIApi } from 'openai';
 import { z } from 'zod';
 
 export const actions: Actions = {
-	async default(event): Promise<ActionFailure<{ error: string; values; errors? }>> {
+	async default(event) {
 		const { supabaseClient } = await getSupabase(event);
 		const { request } = event;
 		const formData = await request.formData();
 
 		const credentials = z.object({
-			email: z
-				.string({ required_error: 'Email is required.' })
-				.email({ message: 'Please enter a valid email.' }),
-			password: z
-				.string({ required_error: 'Password is required.' })
-				.min(6, { message: 'Password must be at least 6 characters.' }),
+			prompt: z.string({ required_error: 'Prompt is required.' }),
 		});
 
 		const validatedData = credentials.safeParse(Object.fromEntries(formData));
 
 		if (!validatedData.success) {
 			return fail(400, {
-				error: 'Make sure you enter a valid email and a password of at least 6 characters.',
+				error: 'The prompt is invalid. Make sure it is within the allowed character limit.',
 				errors: validatedData.error.flatten().fieldErrors,
 				values: {
-					email: formData.get('email') as string,
+					prompt: formData.get('prompt') as string,
 				},
 			});
 		}
 
-		// attempt to log in
-		const { error } = await supabaseClient.auth.signInWithPassword(validatedData.data);
+		const configuration = new Configuration({
+			apiKey: SECRET_OPENAI_KEY,
+		});
+		const openai = new OpenAIApi(configuration);
 
-		if (error) {
-			if (error instanceof AuthApiError && error.status === 400) {
-				return fail(400, {
-					error: 'Incorrect credentials.',
-					values: {
-						email: formData.get('email') as string,
-					},
-				});
-			}
-
+		try {
+			// send prompt to openai
+			const response = await openai.createChatCompletion({
+				model: 'gpt-3.5-turbo',
+				messages: [{ role: 'user', content: formData.get('prompt') as string }],
+			});
+			console.log('aaa', response.data.choices);
+			// return response;
+		} catch (error) {
+			console.log('error', error);
 			return fail(500, {
 				error: 'Server error. Try again later.',
 				values: {
-					email: formData.get('email') as string,
+					prompt: formData.get('prompt') as string,
 				},
 			});
 		}
-
-		// successfully logged in - redirect to homepage
-		throw redirect(307, '/');
 	},
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import type { PageData } from './$types';
+	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
+	import type { PageData, ActionData } from './$types';
 	import { MetaTags } from 'svelte-meta-tags';
 	import meta from '$lib/stores/meta';
 
 	import Button from '$lib/components/ui/Button.svelte';
 	import Waiting from '$lib/components/ui/icons/Waiting.svelte';
+
+	export let data: PageData;
+	export let form: ActionData;
 
 	let isMobile = false;
 	let isGenerating = false;
@@ -19,7 +23,14 @@
 	$: charactersPerLine = isMobile ? 70 : 165;
 	$: textareaHeight = 50 + Math.floor(currentPrompt.length / charactersPerLine) * 30;
 
-	export let data: PageData;
+	const handleSubmit: SubmitFunction = () => {
+		isGenerating = true;
+
+		return async ({ result }) => {
+			isGenerating = false;
+			await applyAction(result);
+		};
+	};
 </script>
 
 <MetaTags title="Home" description="Home Description." {...meta} />
@@ -34,7 +45,7 @@
 </div>
 <p class="tailwind-demo text-center text-2xl font-semibold">Welcome to T3 SvelteKit</p>
 
-<form>
+<form method="post" use:enhance={handleSubmit}>
 	<div
 		class="relative flex w-full flex-grow flex-col rounded-md border border-black/10 bg-white py-2 shadow-[0_0_10px_rgba(0,0,0,0.10)] md:py-3 md:pl-4"
 	>
@@ -43,6 +54,7 @@
 			style="max-height: 200px; height: {textareaHeight}px; overflow-y: hidden;"
 			rows="1"
 			class="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 pl-2 focus:ring-0 focus-visible:ring-0 md:pl-0"
+			name="prompt"
 			bind:value={currentPrompt}
 		/>
 		{#if isGenerating}
@@ -56,6 +68,7 @@
 			</Button>
 		{:else}
 			<Button
+				type="submit"
 				classes="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 hover:bg-gray-100 disabled:hover:bg-transparent disabled:bottom-0.5 md:disabled:bottom-1 right-1 md:right-2"
 			>
 				<svg
